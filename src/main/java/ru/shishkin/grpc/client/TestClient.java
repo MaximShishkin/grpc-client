50ce502ad0615ab6f9b486885acef1cc49a876c4
MaximShishkin <yacyfra@gmail.com>
2023-09-11 16:41:57 +0300

    update package

---
+++ src/main/java/ru/shishkin/grpc/client/TestClient.java
@@ -0,0 +1,25 @@
+package ru.shishkin.grpc.client;
+
+import com.example.grpc.GreetingServiceGrpc;
+import com.example.grpc.GreetingServiceOuterClass;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class TestClient {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+                .usePlaintext().build();
+
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub =
+                GreetingServiceGrpc.newBlockingStub(channel);
+
+        GreetingServiceOuterClass.HelloRequest request = GreetingServiceOuterClass.HelloRequest
+                .newBuilder().setName("Max").build();
+        
+        GreetingServiceOuterClass.HelloResponse response = stub.greeting(request);
+
+        System.out.println(response);
+
+        channel.shutdown();
+    }
+}
